1e77a1ba587c2906ec5fb7ec5ef82f63e7b42091
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-06-08 08:23:52 -0300

    100DaysofCode_day01

---
+++ Day01/main.py
@@ -0,0 +1,6 @@
+# Dia 1 nome da Banda de Rock, iremos gerar nome de uma banda juntanto nome da cidade que nasceu com nome de seu Pet.
+
+name1 = input("Qual o nome da cidade que você nasceu?\n ")
+name2 = input("Qual o nome de seu animal de estimação?\n ")
+name3 = name1 + " " + name2
+print("O nome de sua banda é: " + name3.title())
