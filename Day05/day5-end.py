0b9f59ab2b097acd40e0436845f2411fb260cfd5
Jefferson Seno <jeffersonseno@datacake.com.br>
2022-07-16 11:27:38 -0300

    100DaysOfCode - day05 Ended

--- Day05/day5-end.py
+++ Day05/day5-end.py
@@ -1,6 +1,9 @@
-# For loop with range
-
-total = 0
 for number in range(1, 101):
-    total += number
-print(total)
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
